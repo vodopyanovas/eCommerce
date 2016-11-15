4069c03353b5f2bfdb5f3ea642f2c252a3c6e553
Anton Vodopyanov <vodopyanovas@ya.ru>
2016-11-15 09:43:38 +0300

    Edits

--- shop_app/models.py
+++ shop_app/models.py
@@ -11,7 +11,7 @@ class ProductOptions(models.Model):
     option_group_id = models.ForeignKey('OptionsGroups')  # 1 - color, 2 - size
     option_name = models.CharField(max_length=20)  # red, blue, M, S
     made_in = models.CharField(max_length=50)  # China, USA, Turkey ...
-    fabrics = models.CharField(max_length=150)  # 87% wool, 10% polyamide, 3% elastane
+    fabrics = models.CharField(max_length=100)  # 87% wool, 10% polyamide, 3% elastane
 
 
 class OptionsGroups(models.Model):
@@ -28,13 +28,13 @@ class Product(models.Model):
     brand = models.ForeignKey('Brand')
     product_name = models.CharField(max_length=100)
     product_description_short = models.CharField(max_length=600)
-    product_description_long = models.TextField()
+    product_description_long = models.TextField(null=True, blank=True)
     category_id = models.ForeignKey('Category')
     unit_price = models.DecimalField(max_digits=9, decimal_places=2)
-    discount = models.DecimalField(max_digits=2, decimal_places=2)
-    product_weight = models.DecimalField(max_digits=3, decimal_places=3)
-    product_stock = models.PositiveSmallIntegerField()
-    products_ordered = models.PositiveSmallIntegerField()
+    discount = models.DecimalField(max_digits=3, decimal_places=2)
+    product_weight = models.DecimalField(max_digits=6, decimal_places=3)
+    product_stock = models.PositiveSmallIntegerField(default=0)
+    products_ordered = models.PositiveSmallIntegerField(default=0)
     product_available = models.BooleanField(default=True)
     discount_available = models.BooleanField()
     is_bestseller = models.BooleanField()
