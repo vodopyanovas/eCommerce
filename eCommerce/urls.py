b5722aab4f6526123f5e412d7dbbe60e4a618b3d
Anton Vodopyanov <vodopyanovas@ya.ru>
2016-11-29 14:12:03 +0300

    added frontend to project

--- eCommerce/urls.py
+++ eCommerce/urls.py
@@ -16,11 +16,24 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from shop_app.views import index, product_attributes, products
+from shop_app.views import \
+    index, product_attributes, products,\
+    no_page, cart, checkout, wishlist, contact, product, product_detail, account
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
-    url(r'^attributes/', product_attributes, name='attributes'),
-    url(r'^products/', products, name='products'),
+    url(r'^error/', no_page, name='no_page'),
+    url(r'^cart/', cart, name='cart'),
+    url(r'^checkout/', checkout, name='checkout'),
+    url(r'^wishlist/', wishlist, name='wishlist'),
+    url(r'^contact/', contact, name='contact'),
+    url(r'^product/', product, name='product'),
+    url(r'^product-detail/', product_detail, name='product_detail'),
+    url(r'^account/', account, name='account'),
+
+
+
+    # url(r'^attributes/', product_attributes, name='attributes'),
+    # url(r'^products/', products, name='products'),
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -9,91 +9,176 @@ from django.db import transaction
 # Create your views here.
 def index(request):
     if request.method == 'GET':
-        form = {
-            'product_form': ProductForm(),
-            'product_options': ProductOptionsForm(),
-            # 'image': TestImageForm(),
-        }
-        return render(request, 'shop_app/index.html', form)
+        # form = {
+        #     'product_form': ProductForm(),
+        #     'product_options': ProductOptionsForm(),
+        #     # 'image': TestImageForm(),
+        # }
+        # return render(request, 'shop_app/index.html', form)
+        return render(request, 'shop_app_2/index.html')
 
     elif request.method == 'POST':
-        product_form = ProductForm(request.POST)
-        options_form = ProductOptionsForm(request.POST)
-        # image = TestImageForm(request.POST)
-
-        if product_form.is_valid():
-            with transaction.atomic():
-                product_form.save()
-
-        elif options_form.is_valid():
-            with transaction.atomic():
-                options_form.save()
+        # product_form = ProductForm(request.POST)
+        # options_form = ProductOptionsForm(request.POST)
+        # # image = TestImageForm(request.POST)
+        #
+        # if product_form.is_valid():
+        #     with transaction.atomic():
+        #         product_form.save()
+        #
+        # elif options_form.is_valid():
+        #     with transaction.atomic():
+        #         options_form.save()
 
         # elif image.is_valid():
         #     with transaction.atomic():
         #         image.save()
 
-        form = {
-            'product_form': ProductForm(),
-            'product_options': ProductOptionsForm(),
-            # 'image': TestImageForm(),
-        }
-
-        return render(request, 'shop_app/index.html', form)
+        # form = {
+        #     'product_form': ProductForm(),
+        #     'product_options': ProductOptionsForm(),
+        #     # 'image': TestImageForm(),
+        # }
+        #
+        # return render(request, 'shop_app/index.html', form)
+        return render(request, 'shop_app_2/index.html')
 
     return HttpResponse(status=405)
+
+
+def no_page(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/404.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/404.html')
+
+    return HttpResponse(status=405)
+
+
+def cart(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/cart.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/cart.html')
+
+    return HttpResponse(status=405)
+
+
+def checkout(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/checkout.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/checkout.html')
+
+    return HttpResponse(status=405)
+
+
+def wishlist(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/wishlist.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/wishlist.html')
+
+    return HttpResponse(status=405)
+
+
+def contact(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/contact.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/contact.html')
+
+    return HttpResponse(status=405)
+
+
+def product(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/product.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/product.html')
+
+    return HttpResponse(status=405)
+
+
+def product_detail(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/product-detail.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/product-detail.html')
+
+    return HttpResponse(status=405)
+
+
+def account(request):
+    if request.method == 'GET':
+        return render(request, 'shop_app_2/account.html')
+
+    elif request.method == 'POST':
+        return render(request, 'shop_app_2/account.html')
+
+    return HttpResponse(status=405)
+
 
 
 def product_attributes(request):
-    if request.method == 'GET':
-        form = {
-            'brand': BrandForm(),
-            'category': CategoryForm(),
-            'options': OptionsForm,
-            'options_groups': OptionsGroupsForm(),
-        }
-        return render(request, 'shop_app/attributes.html', form)
-
-    elif request.method == 'POST':
-
-        brand_form = BrandForm(request.POST)
-        category_form = CategoryForm(request.POST)
-        options_form = OptionsForm(request.POST)
-        options_groups_form = OptionsGroupsForm(request.POST)
-
-        if brand_form.is_valid():
-            with transaction.atomic():
-                brand_form.save()
-
-        elif category_form.is_valid():
-            with transaction.atomic():
-                category_form.save()
-
-        elif options_form.is_valid():
-            with transaction.atomic():
-                options_form.save()
-
-        elif options_groups_form.is_valid():
-            with transaction.atomic():
-                options_groups_form.save()
-
-        form = {
-            'brand': BrandForm(),
-            'category': CategoryForm(),
-            'options': OptionsForm,
-            'options_groups': OptionsGroupsForm(),
-        }
-        return render(request, 'shop_app/attributes.html', form)
-    return HttpResponse(status=405)
+    # if request.method == 'GET':
+    #     form = {
+    #         'brand': BrandForm(),
+    #         'category': CategoryForm(),
+    #         'options': OptionsForm,
+    #         'options_groups': OptionsGroupsForm(),
+    #     }
+    #     return render(request, 'shop_app/attributes.html', form)
+    #
+    # elif request.method == 'POST':
+    #
+    #     brand_form = BrandForm(request.POST)
+    #     category_form = CategoryForm(request.POST)
+    #     options_form = OptionsForm(request.POST)
+    #     options_groups_form = OptionsGroupsForm(request.POST)
+    #
+    #     if brand_form.is_valid():
+    #         with transaction.atomic():
+    #             brand_form.save()
+    #
+    #     elif category_form.is_valid():
+    #         with transaction.atomic():
+    #             category_form.save()
+    #
+    #     elif options_form.is_valid():
+    #         with transaction.atomic():
+    #             options_form.save()
+    #
+    #     elif options_groups_form.is_valid():
+    #         with transaction.atomic():
+    #             options_groups_form.save()
+    #
+    #     form = {
+    #         'brand': BrandForm(),
+    #         'category': CategoryForm(),
+    #         'options': OptionsForm,
+    #         'options_groups': OptionsGroupsForm(),
+    #     }
+    #     return render(request, 'shop_app/attributes.html', form)
+    # return HttpResponse(status=405)
+    pass
 
 
 def products(request):
-    if request.method == 'GET':
-        all_products = Product.objects.all()
-
-        all_options = ProductOptions.objects.all()
-
-        # group =
-
-        return render(request, 'shop_app/products.html', {'all_prod': all_products, 'all_opt': all_options})
-    return HttpResponse(status=405)
+    # if request.method == 'GET':
+    #     all_products = Product.objects.all()
+    #
+    #     all_options = ProductOptions.objects.all()
+    #
+    #     # group =
+    #
+    #     return render(request, 'shop_app/products.html', {'all_prod': all_products, 'all_opt': all_options})
+    # return HttpResponse(status=405)
+    pass
